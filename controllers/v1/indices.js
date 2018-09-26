defa91eb2ad0b3e5c068f094061babaf3e30a0b3
SonicMaster455 <jordy@pintu.co.id>
2018-09-26 16:31:25 +0700

    try catch

--- controllers/v1/indices.js
+++ controllers/v1/indices.js
@@ -4,42 +4,55 @@ const { httpStatus, errorCodes } = require('./../../configs/codes');
 
 const indicesController = {
   get: async (req, res) => {
-    req.checkQuery({
-      period: { notEmpty: true, errorMessage: 'period field is required' },
-      symbol: { notEmpty: true, errorMessage: 'symbol field is required' }
-    });
+    try {
+      req.checkQuery({
+        period: { notEmpty: true, errorMessage: 'period field is required' },
+        symbol: { notEmpty: true, errorMessage: 'symbol field is required' }
+      });
 
-    const errors = req.validationErrors();
-    if (errors) {
-      return res.status(httpStatus.badRequest).json({
-        status: httpStatus.badRequest,
+      const errors = req.validationErrors();
+      if (errors) {
+        return res.status(httpStatus.badRequest).json({
+          status: httpStatus.badRequest,
+          success: false,
+          message: errors,
+          code: errorCodes.badRequest
+        });
+      }
+
+      const options = {
+        url: `https://apiv2.bitcoinaverage.com/indices/global/history/${req.query.symbol}?period=${req.query.period}&format=json`,
+        method: 'GET',
+        json: true
+      };
+
+      const result = await request(options);
+      let finalResult = result;
+
+      if (req.query.interval) {
+        const temp = [];
+        for (let index = 0; index < result.length; index += 1) {
+          const set = result[index].time.substr(14, 2);
+          if (set % req.query.interval === 0) {
+            temp.push(result[index]);
+          }
+        }
+        finalResult = temp;
+      }
+
+      return res.status(httpStatus.ok).json({
+        status: httpStatus.ok,
+        success: true,
+        data: finalResult
+      });
+    } catch (e) {
+      return res.status(httpStatus.internalServerError).json({
+        status: httpStatus.internalServerError,
         success: false,
-        message: errors,
-        code: errorCodes.badRequest
+        message: e.message,
+        code: errorCodes.internalServerError
       });
     }
-
-    const options = {
-      url: `https://apiv2.bitcoinaverage.com/indices/global/history/${req.query.symbol}?period=${req.query.period}&format=json`,
-      method: 'GET',
-      json: true
-    };
-
-    const result = await request(options);
-
-    const temp = [];
-    for (let index = 0; index < result.length; index += 1) {
-      const set = result[index].time.substr(14, 2);
-      if (set % req.query.interval === 0) {
-        temp.push(result[index]);
-      }
-    }
-
-    return res.status(httpStatus.ok).json({
-      status: httpStatus.ok,
-      success: true,
-      data: temp
-    });
   }
 };
 
