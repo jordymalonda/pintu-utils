7a60b4b0d21e1ed53f91dff2ef4c9b2dcb2228f7
SonicMaster455 <jordy@pintu.co.id>
2018-10-02 00:58:08 +0700

    takeQuote

--- controllers/v1/circle.js
+++ controllers/v1/circle.js
@@ -126,6 +126,63 @@ const controller = {
         code: errorCodes.internalServerError
       });
     }
+  },
+  takeQuote: async (req, res) => {
+    try {
+      req.checkBody({
+        quote_id: { notEmpty: true, errorMessage: 'quote_id field is required' },
+      });
+
+      const errors = req.validationErrors();
+      if (errors) {
+        return res.status(httpStatus.badRequest).json({
+          status: httpStatus.badRequest,
+          success: false,
+          message: errors,
+          code: errorCodes.badRequest
+        });
+      }
+      const milliseconds = new Date().getTime();
+
+      const bodyData = {
+        url: `${config.get('CIRCLE_HOST')}/takeQuote`,
+        nonce: milliseconds,
+        quote_id: req.body.quote_id
+      };
+
+      const apiSecret = config.get('CIRCLE_SECRET');
+      const signature = crypto.createHmac('sha256', apiSecret).update(JSON.stringify(bodyData)).digest('base64');
+
+      const headerData = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'X-CRCL-APIKEY': config.get('CIRCLE_KEY'),
+        'X-CRCL-SIGNATURE': signature
+      };
+
+      const options = {
+        url: `${config.get('CIRCLE_HOST')}/takeQuote`,
+        method: 'POST',
+        json: true,
+        headers: headerData,
+        body: bodyData
+      };
+
+      const result = await request(options);
+
+      return res.status(httpStatus.ok).json({
+        status: httpStatus.ok,
+        success: true,
+        data: result
+      });
+    } catch (e) {
+      return res.status(httpStatus.internalServerError).json({
+        status: httpStatus.internalServerError,
+        success: false,
+        message: e.message,
+        code: errorCodes.internalServerError
+      });
+    }
   }
 };
 
@@ -133,4 +190,5 @@ const controller = {
 module.exports = (router) => {
   router.get('/', controller.getTrades);
   router.post('/newQuote', controller.newQuote);
+  router.post('/takeQuote', controller.takeQuote);
 };
